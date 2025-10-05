635d263d770f37e133bbdc8b6ba730a962be667c
Vadim Lopatiuk <qnub@users.noreply.github.com.>
2025-10-06 01:39:52 +0400

    Clear and format code

--- container/build/scripts/pre-init.d/90_configure_kick.sh
+++ container/build/scripts/pre-init.d/90_configure_kick.sh
@@ -8,7 +8,6 @@ fi
 sed -i "s|\#include $NGINX_CONFD_DIR/transformers/transcode.conf|include $NGINX_CONFD_DIR/transformers/transcode.conf|g" "$NGINX_APP_CONF_FILE"
 
 # App
-sed -i "s|FFMPEG_LOGLEVEL|$FFMPEG_LOGLEVEL|g" "${NGINX_CONFD_DIR}/apps/kick.conf"
 sed -i "s|KICK_URL|$KICK_URL|g" "${NGINX_CONFD_DIR}/apps/kick.conf"
 sed -i "s|KICK_KEY|$KICK_KEY|g" "${NGINX_CONFD_DIR}/apps/kick.conf"
 
