2b2ffad499608be1dd51510f4f8219e741ebd427
Vadim Lopatiuk <qnub@users.noreply.github.com.>
2025-10-05 22:11:53 +0400

    Simplify app.conf

--- container/build/scripts/pre-init.d/90_configure_kick.sh
+++ container/build/scripts/pre-init.d/90_configure_kick.sh
@@ -8,6 +8,7 @@ fi
 sed -i "s|\#include $NGINX_CONFD_DIR/transformers/transcode.conf|include $NGINX_CONFD_DIR/transformers/transcode.conf|g" "$NGINX_APP_CONF_FILE"
 
 # App
+sed -i "s|RELAY_SECRET|$RELAY_SECRET|g" "${NGINX_CONFD_DIR}/apps/kick.conf"
 sed -i "s|KICK_URL|$KICK_URL|g" "${NGINX_CONFD_DIR}/apps/kick.conf"
 sed -i "s|KICK_KEY|$KICK_KEY|g" "${NGINX_CONFD_DIR}/apps/kick.conf"
 
